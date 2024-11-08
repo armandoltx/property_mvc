a9f647b97c7b8121a90e05e887b389486c132a2b
Armando Ruiz <armando.ruiz@industrie.co>
2024-11-08 14:47:39 +1100

    21. 22. Creando el controller usuarioController, con sus rutas y sus vistas

---
+++ controllers/usuarioController.js
@@ -0,0 +1,17 @@
+
+const formularioLogin = (req, res) => {
+  res.render('auth/login', {
+    autenticado: true
+  }) //como le hemos dicho q las vistas estan en la carpeta views en index.js solo hay q poner el path sin esa carperta
+}
+
+const formularioRegistro = (req, res) => {
+  res.render('auth/registro', {
+
+  })
+}
+
+export {  // es un export nombrado, hay q usar llaves y el mimso nombre cuando lo importas => import { formularioLogin } from '../../'
+  formularioLogin,
+  formularioRegistro
+}
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,28 +1,11 @@
 import express from "express"
+import {formularioLogin, formularioRegistro} from "../controllers/usuarioController.js"
 
 const router = express.Router()
 
-// router.get('/', (req, res) => {
-//   res.send('hello world')
-// })
-
-// router.post('/', (req, res) => {
-//   res.send('hello world POST usar Postman')
-// })
-
-// es lo mismo q lo de arriba pero asi ahorramos espacio
-router.route('/login')
-  .get((req, res) => {
-    res.render('auth/login') //como le hemos dicho q las vistas estan en la carpeta views en index.js solo hay q poner el path sin esa carperta
-  })
-
-  .post((req, res) => {
-    res.send('hello world POST usar Postman')
-  })
+router.get('/login', formularioLogin)
+router.get('/registro', formularioRegistro)
 
 
-router.get('/nosotros', (req, res) => {
-  res.json({msg: "Hola mundo desde nosotros"})
-})
 
 export default router // cuando es export default lo podemos importar en otras partes con cualquier
