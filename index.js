ff868d09b16d6955bb1d363a3f86af867c2e5f26
Armando Ruiz <armando.ruiz@industrie.co>
2024-11-08 11:11:49 +1100

    13. Creando un servidor con express usando require y creando las rutas para los endpoints

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('Hola Mundo')
-console.log(2+2)
+const express = require('express') // extraemos el paquete usamos commonJS
+
+// crea la funcion
+const app = express() // lo asignamos a la var app
+
+// routing los diferentes end points que soporta nuestra app.
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// Definir un puerto y arrancar el proyecto
+const port= 4000
+
+// escucha o conectate en el puerto y corre el callback
+app.listen(port, () => {
+  console.log(`El servidor esta funcionando en el puerto ${port}`)
+})
