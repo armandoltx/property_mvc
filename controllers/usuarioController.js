1282a94b6ee162ca6629ccb1d184217a349d7fae
Armando Ruiz <armando.ruiz@industrie.co>
2024-11-12 15:26:28 +1100

    39. 40. 41. Agregando validaciones al formulario de registro y mostrando los errores en la vista

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from '../models/Usuario.js'
 
 const formularioLogin = (req, res) => {
@@ -17,6 +18,26 @@ const formularioRegistro = (req, res) => {
 const registrar = async(req, res) => {
   // console.log('registrando...') // imprime en el terminal
   // console.log(req.body)
+
+      // Validación
+    await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req)
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+    await check('password').isLength({ min: 6 }).withMessage('El Password debe ser de al menos 6 caracteres').run(req)
+    await check('repetir_password').equals('password').withMessage('Los Passwords no son iguales').run(req)
+
+    let resultado = validationResult(req)
+    // res.json(resultado.array())
+
+    // Verificar que el resultado este vacio
+    if(!resultado.isEmpty()) {
+      // Errores
+      return res.render('auth/registro', {
+        pagina: 'Crear Cuenta',
+        errores: resultado.array()
+      })
+    }
+
+
 
   const usuario = await Usuario.create(req.body)
   res.json(usuario)
