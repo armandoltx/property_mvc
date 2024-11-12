ef4e88c141db1a43e13aa1da27274177574107cf
Armando Ruiz <armando.ruiz@industrie.co>
2024-11-12 15:47:37 +1100

    43. Evitar usuarios duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -19,28 +19,42 @@ const registrar = async(req, res) => {
   // console.log('registrando...') // imprime en el terminal
   // console.log(req.body)
 
-      // Validación
-    await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req)
-    await check('email').isEmail().withMessage('Eso no parece un email').run(req)
-    await check('password').isLength({ min: 6 }).withMessage('El Password debe ser de al menos 6 caracteres').run(req)
-    await check('repetir_password').equals('password').withMessage('Los Passwords no son iguales').run(req)
+  // Validación
+  await check('nombre').notEmpty().withMessage('El Nombre no puede ir vacio').run(req)
+  await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+  await check('password').isLength({ min: 6 }).withMessage('El Password debe ser de al menos 6 caracteres').run(req)
+  await check('repetir_password').equals(req.body.password).withMessage('Los Passwords no son iguales').run(req)
 
-    let resultado = validationResult(req)
-    // res.json(resultado.array())
+  let resultado = validationResult(req)
+  // res.json(resultado.array())
 
-    // Verificar que el resultado este vacio
-    if(!resultado.isEmpty()) {
-      // Errores
-      return res.render('auth/registro', {
-        pagina: 'Crear Cuenta',
-        errores: resultado.array(),
-        usuario: {
-          nombre: req.body.nombre,
-          email: req.body.email
-        }
-      })
-    }
+  // Verificar que el resultado este vacio
+  if(!resultado.isEmpty()) {
+    // Errores
+    return res.render('auth/registro', {
+      pagina: 'Crear Cuenta',
+      errores: resultado.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email
+      }
+    })
+  }
 
+  // Verificar que el usuario no este duplicado
+  const existeUsuario = await Usuario.findOne( { where: { email: req.body.email } } )
+  // console.log(existeUsuario)
+  if(existeUsuario) {
+    return res.render('auth/registro', {
+      pagina: 'Crear Cuenta',
+      errores: [{ msg: 'El email ya existe' }],
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email
+      }
+    })
+  }
+  // return
 
 
   const usuario = await Usuario.create(req.body)
