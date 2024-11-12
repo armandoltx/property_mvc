b64932fcfb1548820d43988ee021ab5d0d9f18ef
Armando Ruiz <armando.ruiz@industrie.co>
2024-11-12 15:31:44 +1100

    42. Autollenando los campos q pasaron la validacion

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -33,7 +33,11 @@ const registrar = async(req, res) => {
       // Errores
       return res.render('auth/registro', {
         pagina: 'Crear Cuenta',
-        errores: resultado.array()
+        errores: resultado.array(),
+        usuario: {
+          nombre: req.body.nombre,
+          email: req.body.email
+        }
       })
     }
 
