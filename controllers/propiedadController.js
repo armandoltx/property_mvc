2506f2c49ed7c3785a920c6783c2211bd125ff7b
Armando Ruiz <armando.ruiz@industrie.co>
2024-11-20 10:31:02 +1100

    68. 69. 70. creando el formulario para crear propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -8,7 +8,17 @@ const admin = (req, res) => {
   })
 }
 
+// Formulario para crear una nueva propiedad
+const crear = (req, res) => {
+  // console.log('desde crear')
+  res.render('propiedades/crear', {
+    pagina: 'Crear Propiedad',
+    barra: true
+  })
+}
+
 
 export {  // es un export nombrado, hay q usar llaves y el mimso nombre cuando lo importas => import { formularioLogin } from '../../'
-  admin
+  admin,
+  crear
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {admin} from "../controllers/propiedadController.js"
+import { admin, crear } from "../controllers/propiedadController.js"
+
 const router = express.Router()
 
 router.get('/mis-propiedades', admin)
+router.get('/propiedades/crear', crear)
 
 
 export default router // cuando es export default lo podemos importar en otras partes con cualquier
