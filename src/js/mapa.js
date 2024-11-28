ef2fe56faf5fdc55d0ceb02bbf676ebe16860a9d
armando ltx <armandoltx@armandos-Air-2.modem>
2024-11-28 14:21:16 +1100

    75. agregando un pin con la ubicacion

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,12 +1,17 @@
 (function() {
-    const lat = -33.8716419;
-    const lng = 151.2059691;
-    const mapa = L.map('mapa').setView([lat, lng ], 16);
+  const lat = -33.8716419;
+  const lng = 151.2059691;
+  const mapa = L.map('mapa').setView([lat, lng], 16);
+  let marker;
 
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(mapa);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mapa);
-
-
+  // El Pin
+  marker = new L.marker([lat, lng], {
+    draggable: true,
+    autoPan: true,
+  }).addTo(mapa);
 })()
