d190b339bf6a954f83be4347f26a775efbb68eed
armando ltx <armandoltx@armandos-Air-2.modem>
2024-11-29 11:33:57 +1100

    91. 92. Agregando las validaciones del formulario

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,35 +1,63 @@
-import Precio from "../models/Precio.js"
-import Categoria from "../models/Categoria.js"
+import { validationResult } from 'express-validator';
+import Precio from '../models/Precio.js';
+import Categoria from '../models/Categoria.js';
 
 const admin = (req, res) => {
   // res.send('Mis propiedades')
   res.render('propiedades/admin', {
     pagina: 'Mis Propiedades',
-    barra: true
-  })
-}
+    barra: true,
+  });
+};
 
 // Formulario para crear una nueva propiedad
 const crear = async (req, res) => {
-  // console.log('desde crear')
+  console.log('desde crear')
 
   // Consultar Modelo de Precio y Categoria
-  const [categorias, precios] = await Promise.all([ // await para que termine el proceso antes de pasar a la siguiente linea
+  const [categorias, precios] = await Promise.all([
+    // await para que termine el proceso antes de pasar a la siguiente linea
     Categoria.findAll(),
-    Precio.findAll()
-  ])
-
+    Precio.findAll(),
+  ]);
 
   res.render('propiedades/crear', {
     pagina: 'Crear Propiedad',
     barra: true,
+    csrfToken: req.csrfToken(),
     categorias,
-    precios
-  })
+    precios,
+  });
+};
+
+const guardar = async (req, res) => {
+  console.log("en guardar...")
+
+  // Validacion que viene desde la ruta
+  let resultado = validationResult(req)
+  // Verificar que el resultado este vacio
+  if(!resultado.isEmpty()) {
+    // Consultar Modelo de Precio y Categoria
+    const [categorias, precios] = await Promise.all([
+      // await para que termine el proceso antes de pasar a la siguiente linea
+      Categoria.findAll(),
+      Precio.findAll(),
+    ]);
+    // Errores
+    return res.render('propiedades/crear', {
+      pagina: 'Crear Propiedad',
+      barra: true,
+      csrfToken: req.csrfToken(),
+      categorias,
+      precios,
+      errores: resultado.array(),
+    });
+  };
 }
 
-
-export {  // es un export nombrado, hay q usar llaves y el mimso nombre cuando lo importas => import { formularioLogin } from '../../'
+export {
+  // es un export nombrado, hay q usar llaves y el mimso nombre cuando lo importas => import { formularioLogin } from '../../'
   admin,
-  crear
+  crear,
+  guardar,
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,10 +1,28 @@
 import express from "express"
-import { admin, crear } from "../controllers/propiedadController.js"
+import { body } from 'express-validator'
+import { admin, crear, guardar } from '../controllers/propiedadController.js';
 
 const router = express.Router()
 
 router.get('/mis-propiedades', admin)
 router.get('/propiedades/crear', crear)
+router.post(
+  '/propiedades/crear',
+  // aqui es donde agregamos las validaciones en las rutas. tb pueden estar en el controlador.
+  body('titulo').notEmpty().withMessage('El titulo del Anuncio es Obligatorio'),
+  body('descripcion')
+    .notEmpty()
+    .withMessage('La descripcion no puede ir vacia')
+    .isLength({ max: 200 })
+    .withMessage('La descripcion es muy larga'),
+  body('categoria').isNumeric().withMessage('Selecciona una categoria'), // pq estamos usando el id
+  body('precio').isNumeric().withMessage('Selecciona un rango de precios'), // pq estamos usando el id
+  body('habitaciones').isNumeric().withMessage('Selecciona un numero de habitaciones'), // pq estamos usando el id
+  body('estacionamiento').isNumeric().withMessage('Selecciona un numero de estacionamientos'), // pq estamos usando el id
+  body('wc').isNumeric().withMessage('Selecciona un numero de wc.'), // pq estamos usando el id
+  body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
+  guardar
+);
 
 
 export default router // cuando es export default lo podemos importar en otras partes con cualquier
